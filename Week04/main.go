fa91c501c7feb4e1f02729ace4d1ce3ce5f60a45
pg719 <pg719@qq.com>
2020-12-16 15:01:03 +0800

    x

---
+++ Week04/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"github.com/php403/GO-001/Week04/"
+	"github.com/php403/GO-001/Week04/redis"
+	"github.com/php403/GO-001/Week04/service"
+	"github.com/php403/GO-001/Week04/transport"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+)
+
+func main()  {
+
+
+	var (
+		// 服务地址和服务名
+		servicePort = flag.Int("service.port", 10086, "service port")
+
+	)
+
+	flag.Parse()
+
+	ctx := context.Background()
+	errChan := make(chan error)
+
+	err := dao.InitMysql("127.0.0.1", "3306", "root", "xuan", "user")
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	err = redis.InitRedis("127.0.0.1","6379", "" )
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	userService := service.MakeUserServiceImpl(&dao.UserDAOImpl{})
+
+	userEndpoints := &endpoint.UserEndpoints{
+		endpoint.MakeRegisterEndpoint(userService),
+		endpoint.MakeLoginEndpoint(userService),
+	}
+
+	r := transport.MakeHttpHandler(ctx, userEndpoints)
+
+
+
+	go func() {
+		errChan <- http.ListenAndServe(":"  + strconv.Itoa(*servicePort), r)
+	}()
+
+	go func() {
+		// 监控系统信号，等待 ctrl + c 系统信号通知服务关闭
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+		errChan <- fmt.Errorf("%s", <-c)
+	}()
+
+	error := <-errChan
+	log.Println(error)
+
+}
